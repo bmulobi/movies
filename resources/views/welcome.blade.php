ca45d685cd26e563a88ec5d89369c6ec5be255b3
Bernard Mulobi <bmulobi@gmail.com>
2018-10-17 21:41:50 +0300

    edit home page
    Signed-off-by: Bernard Mulobi <bmulobi@gmail.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,8 @@
             <div class="description">
                 <h1>Hello ,Welcome To My official Website</h1>
                 <p>
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    You can add see all the latest movies and add new categories and movies.
+                    Please register in order to view this details
                 </p>
                 <button class="btn btn-outline-secondary btn-lg">See more</button>
             </div>
